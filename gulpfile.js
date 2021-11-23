b1a5d6a31b4acf6080d7b791180603f96971ddf2
Rael-x <adebolaakomolafe@gmail.com>
2021-11-23 10:23:56 +0100

    Deleted Gulp file

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-const gulpConcat = require("gulp-concat");
-const gulp = require("gulp");
-
-var jsFiles = 'src/ts/**', jsDest = 'src/ts/final/'
-gulp.task('scripts', function () {
-    return gulp.src(jsFiles).pipe(gulpConcat('concord.app.ts', {overwrite: true})).pipe(gulp.dest(jsDest));
-});
